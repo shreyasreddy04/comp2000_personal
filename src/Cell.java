b1e0fdf336065c3e049743615198d222573e3fdc
shreyasreddy04 <shreyasreddy.raichandrashekarreddy@students.mq.edu.au>
2024-08-13 01:32:07 +1000

    files for milestone1task1

--- src/Cell.java
+++ src/Cell.java
@@ -3,30 +3,31 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 public class Cell {
-  int x;
-  int y;
-  static int size = 35;
+    int x;
+    int y;
+    static int size = 35;
 
-  public Cell(int inX, int inY) {
-    x = inX;
-    y = inY;
-  }
-  public void paint(Graphics g, Point mousePos) {
-    if(contains(mousePos)) {
-      g.setColor(Color.GRAY);
-    } else {
-      g.setColor(Color.WHITE);
+    public Cell(int inX, int inY) {
+        x = inX;
+        y = inY;
     }
-    g.fillRect(x, y, size, size);
-    g.setColor(Color.BLACK);
-    g.drawRect(x, y, size, size);
-  }
 
-  public boolean contains(Point p) {
-    if(p != null) {
-      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
-    } else {
-      return false;
+    public void paint(Graphics g, Point mousePos) {
+        if (contains(mousePos)) {
+            g.setColor(Color.GRAY);
+        } else {
+            g.setColor(Color.WHITE);
+        }
+        g.fillRect(x, y, size, size);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, size, size);
     }
-  }
+
+    public boolean contains(Point mousePointer) {
+        if (mousePointer != null) {
+            return x < mousePointer.x && x + size > mousePointer.x && y < mousePointer.y && y + size > mousePointer.y;
+        } else {
+            return false;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,25 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
         Main window = new Main();
-        window.run();
     }
 
     class Canvas extends JPanel {
-        Stage stage;
+        Grid grid = new Grid();
 
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
-            Grid grid = new Grid();
-            Actor[] actors = new Actor[]{
-                new Cat(grid.cells[2][3]),
-                new Dog(grid.cells[4][5]),
-                new Bird(grid.cells[6][7])
-            };
-            stage = new Stage(grid, actors);
         }
 
-        @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            stage.paint(g, getMousePosition());
+            grid.paint(g, getMousePosition());
+            repaint();
         }
     }
 
@@ -38,15 +30,4 @@ public class Main extends JFrame {
         this.pack();
         this.setVisible(true);
     }
-
-    public void run() {
-        while (true) {
-            repaint();
-            try {
-                Thread.sleep(16); // Sleep for 16 ms to achieve ~60 FPS
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-      }
-}
 }
